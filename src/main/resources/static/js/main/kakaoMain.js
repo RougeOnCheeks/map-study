381feb359fed59624acb3cf7ca915b58f9be084d
RougeOnCheeks <lilianaaa804@gmail.com>
2022-07-25 14:05:22 +0900

    지도 이동시키기

--- src/main/resources/static/js/main/kakaoMain.js
+++ src/main/resources/static/js/main/kakaoMain.js
@@ -1,7 +1,24 @@
 var container = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
 var options = { //지도를 생성할 때 필요한 기본 옵션
-    center: new kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
+    center: new kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표. 지도를 생성하는데 반드시 필요
     level: 3 //지도의 레벨(확대, 축소 정도)
 };
 
 var map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
+
+/**
+ * 지도 중심 이동
+ */
+function setCenter(){
+    //이동할 위도 경도 위치 생성
+    var moveLatLon = new kakao.maps.LatLng(37.4783447, 127.0374924);
+    map.setCenter(moveLatLon);
+}
+
+/**
+ * 지도 중심 부드럽게 이동: 지도가 표시되고 있는 영역크기를 벗어나지 않는 거리라면 애니메이션 효과처럼 지도를 부드럽게 이동
+ */
+function panTo(){
+    var moveLatLon = new kakao.maps.LatLng(33.452613, 126.570888);
+    map.panTo(moveLatLon);
+}
