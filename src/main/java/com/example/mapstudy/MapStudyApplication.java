11c9ae844190d63e477755e5565db5d41cb431b3
RougeOnCheeks <lilianaaa804@gmail.com>
2022-07-25 11:15:28 +0900

    initial

---
+++ src/main/java/com/example/mapstudy/MapStudyApplication.java
@@ -0,0 +1,13 @@
+package com.example.mapstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MapStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mapstudy/MapStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mapstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
