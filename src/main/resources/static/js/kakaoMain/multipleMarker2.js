9a7c789e86464c3f2774ec370945b740eeebba4a
RougeOnCheeks <lilianaaa804@gmail.com>
2022-07-28 13:25:59 +0900

    여러개 마커에 이벤트 등록2

---
+++ src/main/resources/static/js/kakaoMain/multipleMarker2.js
@@ -0,0 +1,93 @@
+const MARKER_WIDTH = 33, // 기본, 클릭 마커의 너비
+    MARKER_HEIGHT = 36, // 기본, 클릭 마커의 높이
+    OFFSET_X = 12, // 기본, 클릭 마커의 기준 X좌표
+    OFFSET_Y = MARKER_HEIGHT, // 기본, 클릭 마커의 기준 Y좌표
+    OVER_MARKER_WIDTH = 40, // 오버 마커의 너비
+    OVER_MARKER_HEIGHT = 42, // 오버 마커의 높이
+    OVER_OFFSET_X = 13, // 오버 마커의 기준 X좌표
+    OVER_OFFSET_Y = OVER_MARKER_HEIGHT, // 오버 마커의 기준 Y좌표
+    SPRITE_MARKER_URL = 'https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markers_sprites2.png', // 스프라이트 마커 이미지 URL
+    SPRITE_WIDTH = 126, // 스프라이트 이미지 너비
+    SPRITE_HEIGHT = 146, // 스프라이트 이미지 높이
+    SPRITE_GAP = 10; // 스프라이트 이미지에서 마커간 간격
+
+const markerSize = new kakao.maps.Size(MARKER_WIDTH, MARKER_HEIGHT), // 기본, 클릭 마커의 크기
+    markerOffset = new kakao.maps.Point(OFFSET_X, OFFSET_Y), // 기본, 클릭 마커의 기준좌표
+    overMarkerSize = new kakao.maps.Size(OVER_MARKER_WIDTH, OVER_MARKER_HEIGHT), // 오버 마커의 크기
+    overMarkerOffset = new kakao.maps.Point(OVER_OFFSET_X, OVER_OFFSET_Y), // 오버 마커의 기준 좌표
+    spriteImageSize = new kakao.maps.Size(SPRITE_WIDTH, SPRITE_HEIGHT); // 스프라이트 이미지의 크기
+
+const positions = [
+    new kakao.maps.LatLng(33.44975, 126.56967),
+    new kakao.maps.LatLng(33.450579, 126.56956),
+    new kakao.maps.LatLng(33.4506468, 126.5707)
+];
+let selectedMarker = null;
+
+const mapContainer = document.getElementById('map'),
+    mapOption = {
+        center: new kakao.maps.LatLng(33.450701, 126.570667),
+        level: 3
+    };
+
+const map = new kakao.maps.Map(mapContainer, mapOption);
+
+const createMarkerImage = (markerSize, offset, spriteOrigin) => {
+    const markerImage = new kakao.maps.MarkerImage(
+        SPRITE_MARKER_URL, // 스프라이트 마커 이미지 URL
+        markerSize, // 마커의 크기
+        {
+            offset: offset, // 마커 이미지에서의 기준 좌표
+            spriteOrigin: spriteOrigin, // 스트라이프 이미지 중 사용할 영역의 좌상단 좌표
+            spriteSize: spriteImageSize // 스프라이트 이미지의 크기
+        }
+    );
+    return markerImage;
+}
+
+const addMarker = (position, normalOrigin, overOrigin, clickOrigin) => {
+    const normalImage = createMarkerImage(markerSize, markerOffset, normalOrigin),
+        overImage = createMarkerImage(overMarkerSize, overMarkerOffset, overOrigin),
+        clickImage = createMarkerImage(markerSize, markerOffset, clickOrigin);
+
+    const marker = new kakao.maps.Marker({
+        map: map,
+        position: position,
+        image: normalImage
+    });
+
+    marker.normalImage = normalImage;
+    kakao.maps.event.addListener(marker, 'mouseover', function (){
+        setImage(overImage);
+    });
+
+    kakao.maps.event.addListener(marker, 'mouseout', function (){
+        setImage(normalImage);
+    });
+
+    kakao.maps.event.addListener(marker, 'click', function (){
+        setImage(clickImage);
+        selectedMarker = marker;
+    });
+
+    const setImage = (image) => {
+        if (!selectedMarker || selectedMarker !== marker) {
+            !!selectedMarker && selectedMarker.setImage(selectedMarker.normalImage);
+            marker.setImage(image);
+        }
+    }
+};
+
+for(let i in positions){
+    const gapX = (MARKER_WIDTH + SPRITE_GAP), // 스프라이트 이미지에서 마커로 사용할 이미지 X좌표 간격 값
+        originY = (MARKER_HEIGHT + SPRITE_GAP) * i, // 스프라이트 이미지에서 기본, 클릭 마커로 사용할 Y좌표 값
+        overOriginY = (OVER_MARKER_HEIGHT + SPRITE_GAP) * i, // 스프라이트 이미지에서 오버 마커로 사용할 Y좌표 값
+        normalOrigin = new kakao.maps.Point(0, originY), // 스프라이트 이미지에서 기본 마커로 사용할 영역의 좌상단 좌표
+        clickOrigin = new kakao.maps.Point(gapX, originY), // 스프라이트 이미지에서 마우스오버 마커로 사용할 영역의 좌상단 좌표
+        overOrigin = new kakao.maps.Point(gapX * 2, overOriginY); // 스프라이트 이미지에서 클릭 마커로 사용할 영역의 좌상단 좌표
+
+    addMarker(positions[i], normalOrigin, overOrigin, clickOrigin);
+}
+
+
+
